ad829a6520b9f42a4355534250c689a3ab6e1157
Ken Starzer <kenstarzer@users.noreply.github.com>
2026-07-18 16:21:47 +0000

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,15 @@ function randomNumber(min, max) {
 // Define divs variable as all divs inside grid container
 let divs = document.querySelectorAll(".grid-container div");
 
+// Define Randomize function
+function randomize() {
+    console.log(divs.length, "randomize!");
+
+    // just the grid divs
+    divs.forEach(function (div) {
+        let scale = randomNumber(.5, 1.2);
+        let translateX = randomNumber(0, 50);
+        let translateY = randomNumber(0, 20);
+        let rotate = randomNumber(0, 360);
+    })
+}
